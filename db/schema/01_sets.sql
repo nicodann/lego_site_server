1ba483da0498ed49a9c12813f0e3a435aa37a923
Nico Dann <nicodann@gmail.com>
2024-03-06 19:15:26 -0500

    Create sql seed files.

---
+++ db/schema/01_sets.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS sets CASCADE;
+
+CREATE TABLE sets (
+  id SERIAL PRIMARY KEY NOT NULL,
+  number INT NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  image_url VARCHAR(255),
+  url VARCHAR(255),
+  category VARCHAR(255)
+)
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
 
 app.use(cors());
+app.use(morgan("dev"))
 
 app.get("/api/v1/users", (req,res) => {
   const users = [
